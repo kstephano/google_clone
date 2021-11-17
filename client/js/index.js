ed2cde41c114394a6f71f7873ac1100fbe46352c
Kelvin Stephano <kstephano@gmail.com>
2021-11-17 15:36:42 +0000

    refactored and reformatted code

--- client/js/index.js
+++ client/js/index.js
@@ -1,25 +1,40 @@
 const allPagesButton = document.querySelector('#all-pages-button')
 const randomPageButton = document.querySelector('#random-page-button')
 
+initListeners();
 
-function saveTextInput () {
-        const textInput = document.querySelector('input').value
-        return textInput
+function getSearchInput () {
+    const textInput = document.querySelector('input').value;
+    return textInput;
 }
 
-allPagesButton.addEventListener('click', search)
-randomPageButton.addEventListener('click', randomSearch)
+function initListeners() {
+    allPagesButton.addEventListener('click', search)
+    randomPageButton.addEventListener('click', randomSearch)
+}
+
 
 function search(){
-    let searchedText = saveTextInput()
-    console.log(searchedText)
-    if (searchedText) {
-        if (searchedText.toLowerCase() == "artists") {
+    let query = getSearchInput();
+    console.log(query)
+
+    // if text exists
+    if (query) {
+        if (query.toLowerCase() == "artists") {
             searchAllArtists()
         } else {
             searchForArtist()
         }
     }
+}
+
+async function fetchQuery(e) {
+    e.preventDefault();
+    const response = await fetch(`http://localhost:3000/artists/query${getSearchInput()}`);
+    const data = await response.json();
+
+    // todo
+    // window.location.href = 
 }
 
 function randomSearch(){
