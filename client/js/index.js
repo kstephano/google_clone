56ecc27082b2deac9094790e840d2c8fcd91c4bd
adyrmishi <angeladyrmishi@outlook.com>
2021-11-17 14:01:21 +0000

    functionality for different functions to run depending on user input

--- client/js/index.js
+++ client/js/index.js
@@ -1,28 +1,36 @@
 const allPagesButton = document.querySelector('#all-pages-button')
 const randomPageButton = document.querySelector('#random-page-button')
 
+
 function saveTextInput () {
         const textInput = document.querySelector('input').value
         return textInput
 }
 
-let luckyText = allPagesButton.addEventListener('click', saveTextInput)
-let searchText = randomPageButton.addEventListener('click', saveTextInput)
+allPagesButton.addEventListener('click', search)
+randomPageButton.addEventListener('click', randomSearch)
 
+function search(){
+    let searchedText = saveTextInput()
+    console.log(searchedText)
+    if (searchedText) {
+        if (searchedText.toLowerCase() == "artists") {
+            searchAllArtists()
+        } else {
+            searchForArtist()
+        }
+    }
+}
 
-if (luckyText) {
-    if (luckyText.toLowerCase() === "artists") {
-        // getRandomArtist()
-        console.log("Artist")
-    } else {
-    getArtist(luckyText)
+function randomSearch(){
+    let luckyText = saveTextInput()
+    console.log(luckyText)
+    if (luckyText) {
+        if (luckyText.toLowerCase() == "artists") {
+            console.log('YES')
+            getRandomArtist()
+        } else {
+            getArtist()
+        }
     }
-} else if (searchText) {
-    if (luckyText.toLowerCase() === "artists") {
-        searchAllArtists()
-    } else {
-        searchForArtist(searchText)
-    }
-} else {
-    console.log("dumb")
 }
